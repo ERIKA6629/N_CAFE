852fca0d3b063603c57ab89138520d358861d814
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-03-01 15:08:38 +0000

    [add]admin-log_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }do
+    get 'customers/sign_up' => 'public/registrations#new', as: 'new_customer_registration'
+    post 'customers/sign_up' => 'public/registrations#create', as: 'customer_registration'
+  end
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  scope module: :public do
+    get '/' => 'homes#top', as: 'public_top'
+  end
+  
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
