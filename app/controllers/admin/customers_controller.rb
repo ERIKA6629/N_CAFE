025c1f9c71c240e1cc41d1f420458a197fef2d19
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-03-06 13:59:01 +0000

    [add]admin/customer_edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,6 +9,20 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to admin_customer_path(customer.id)
+    else
+      @customer = customer
+      render :edit
+    end
+  end
+  
   def search
     if Customer.find_by(id: params[:id]).present?
       redirect_to admin_customer_path(params[:id])
@@ -16,6 +30,12 @@ class Admin::CustomersController < ApplicationController
       @customers = Customer.page(params[:page])
       redirect_to admin_customers_path, notice: '検索したIDは見つかりませんでした。'
     end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :email, :is_active)
   end
   
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,9 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
+  
+  def top
+    @reservation_times = ReservationTime.all
+    @seats = Seat.all
+  end
 
 end
