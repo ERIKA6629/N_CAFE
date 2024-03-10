a0f9a8e7518a6cb9b268e4abbed3bad25893acb1
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-03-10 13:22:32 +0000

    [add]customer-reservation-topview

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,17 @@
 class Public::HomesController < ApplicationController
   
   def cafe_information
+    
+    @display_date = Time.current
     @seats = Seat.all
+    if ReservationTime.search_display_time.nil?
+      @reservation_time = ReservationTime.first
+      @open_time = false
+    else
+      @reservation_time = ReservationTime.search_display_time
+    end
+    @reservations = Reservation.all
   end
+  
 
 end
--- app/models/reservation_time.rb
+++ app/models/reservation_time.rb
@@ -1,3 +1,15 @@
 class ReservationTime < ApplicationRecord
   has_many :reservations
+  
+  def self.search_display_time
+    arr = []
+    ReservationTime.all.each do |reservation_time|
+      start_time_obj = Time.zone.parse("#{Date.current.to_s} #{reservation_time.start_at}")
+      end_time_obj = Time.zone.parse("#{Date.current.to_s} #{reservation_time.end_at}")
+      if start_time_obj <= Time.current && Time.current < end_time_obj
+        arr << reservation_time
+      end
+    end
+    arr[0]
+  end
 end
