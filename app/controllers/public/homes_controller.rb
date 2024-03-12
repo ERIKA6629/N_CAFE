6d695d18093b20bb144804a1000c7558689da70e
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-03-12 11:17:37 +0000

    [fix]customer-reservation_view-to_reflect_business_hours

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,7 +6,9 @@ class Public::HomesController < ApplicationController
     @reservation_date = Time.current
     @seats = Seat.all
     @reservation_time = ReservationTime.search_display_time
-    if @reservation_time.business_hours == "after_closing"
+    if @reservation_time.business_hours == "after_closing" && Date.today.wday == 3
+      @reservation_date = Time.current.ago(2.days)
+    elsif @reservation_time.business_hours == "after_closing"
       @reservation_date = Time.current.tomorrow
     end
     @reservations = Reservation.all
--- app/models/reservation_time.rb
+++ app/models/reservation_time.rb
@@ -8,10 +8,14 @@ class ReservationTime < ApplicationRecord
     ReservationTime.all.each do |reservation_time|
       start_time_obj = Time.zone.parse("#{Date.current.to_s} #{reservation_time.start_at}")
       end_time_obj = Time.zone.parse("#{Date.current.to_s} #{reservation_time.end_at}")
-      if Time.current.beginning_of_day < Time.current && Time.current < start_time_obj 
+      if Date.today.wday == 4
+        reservation_time.business_hours = "after_closing"
+        arr << reservation_time
+      elsif Time.current.beginning_of_day < Time.current && Time.current < start_time_obj 
         reservation_time.business_hours = "before_opening"
         arr << reservation_time
       elsif start_time_obj <= Time.current && Time.current < end_time_obj
+        reservation_time.business_hours = "open_n_cafe"
         arr << reservation_time
       elsif end_time_obj <= Time.current && Time.current < Time.current.end_of_day
         reservation_time.business_hours = "after_closing"
