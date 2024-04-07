77d5104ec7360f0fbbb125af193ed430d154035d
ERIKA6629 <151750459+ERIKA6629@users.noreply.github.com>
2024-04-07 14:38:58 +0000

    [add]rayout-admin-menu,genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,13 +3,17 @@ class Admin::GenresController < ApplicationController
   
   def index
     @genres = Genre.all
-      
+    @genre = Genre.new
   end
   
   def create
-    genre = Genre.new(genre_params)
-    genre.save
-    redirect_to admin_genres_path
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
   
   def edit
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ApplicationRecord
   has_one_attached :image
   has_many :menus, -> { order "name DESC" }
+  
+  validates :name, presence: true
+  validates :image, presence: true
 end
