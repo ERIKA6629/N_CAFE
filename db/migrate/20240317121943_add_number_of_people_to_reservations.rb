6e53fb6d651f0830c1a4624f889b4586a3addf1a
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2024-04-13 11:07:57 +0000

    [fix]migration-fileバグ

--- db/migrate/20240317121943_add_number_of_people_to_reservations.rb
+++ db/migrate/20240317121943_add_number_of_people_to_reservations.rb
@@ -1,6 +1,6 @@
 class AddNumberOfPeopleToReservations < ActiveRecord::Migration[6.1]
   def change
-    add_column :reservations, :number_of_people, :interger
+    add_column :reservations, :number_of_people, :integer
     change_column_null :reservations, :number_of_people, false, 0
   end
 end
